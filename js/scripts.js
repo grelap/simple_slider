bc1f13486365965b5dadc20d479ac0686b47b2c3
xenogonx <pablez@wp.pl>
2018-01-12 14:44:40 +0100

    variables cleaning

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,45 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-  let carousel = $("#carousel");
-  let carouselList = carousel.find("ul.photo");
-  let carouselMenu = carousel.find("ul.dots");
-
-  carouselList.find("li").each(function(){
-    carouselMenu.append('<li></li>');
+  const carousel = $("#carousel");
+  const carouselList = carousel.find("ul.photo");
+  const carouselMenu = carousel.find("ul.dots");
+ 
+  carouselList.find("li").each(function() { 
+    carouselMenu.append("<li></li>");
   });
 
-  let dots = carouselMenu.find("li");
+  const dots = carouselMenu.find("li");
   dots.first().addClass("active");
 
-  dots.click(function(){
-    if (!$(this).hasClass("active")){
-      let target = $(this).index();
-      console.log(target);
-      slideChange(target);
+  //zmiana slajdu
+  function slideChange(slide) {
+    carouselList.stop().animate({ "left": -604 * slide });
+    dots.removeClass("active").eq(slide).addClass("active");
+    console.log(slide);
+  };
+
+  //zmiana slajdu kropką
+  dots.click(function() {
+    if (!$(this).hasClass("active")) {
+      const blink = $(this).index();
+      slideChange(blink);
     }
   });
 
-  //zmiana slajdu
-  function slideChange(target){
-    carouselList.stop().animate({'left': -604 * target});
-    dots.removeClass("active").eq(target).addClass("active");
-  };
-
   //prawy guzik
-    $("#btn-right").click(function(){
-      target = dots.siblings(".active").index();
-      target == (dots.length - 1) ? target = 0 : target += 1;
-        slideChange(target);
-      console.log('kliknales prawy guzik');
-    });
+  $("#btn-right").click(function() {
+    let target = dots.siblings(".active").index();
+    target == (dots.length - 1) ? target = 0 : target += 1;
+    slideChange(target);
+    console.log("kliknales prawy guzik");
+  });
 
   //lewy guzik
-    $("#btn-left").click(function(){
-      target = dots.siblings(".active").index();
-      target == 0 ? target = (dots.length - 1) : target -= 1;
-        slideChange(target);
-      console.log('kliknales lewy guzik');
-   });
+  $("#btn-left").click(function() {
+    let target = dots.siblings(".active").index();
+    target == 0 ? target = (dots.length - 1) : target -= 1;
+    slideChange(target);
+    console.log("kliknales lewy guzik");
+  });
 
 });
